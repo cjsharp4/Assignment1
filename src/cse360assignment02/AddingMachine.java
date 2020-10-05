16c29777f4b80d34ac093f0d3c5a30aca323dfc5
carsons <carsons@192.168.1.13>
2020-10-05 16:32:28 -0700

    Finished writing methods

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -10,25 +10,31 @@ package cse360assignment02;
 */
 public class AddingMachine {
   private int total;
+  private String str = "0";
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total = total + value; //adds value to overall total
+	  str = str + " + " + value;
   }
 
   public void subtract (int value) {
+	  total = total - value; //subtracts value from overall total
+	  str = str + " - " + value;
   }
 
   public String toString () {
-    return "";
+    return str; //returns history of transactions
   }
 
   public void clear() {
+	  str = "0"; //clears transactions and resets toString to 0
   }
 }
